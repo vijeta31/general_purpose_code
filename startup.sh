2fb1e1ba10fbcf4b61e383aeb303d71d5bd3fbc0
Vijeta <66792467+vijeta31@users.noreply.github.com>
2025-10-07 03:14:18 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -24,3 +24,5 @@ curl -LsSf https://astral.sh/uv/install.sh | sh && export PATH="/root/.local/bin
 curl -LsSf https://astral.sh/uv/install.sh | sh && export PATH="/root/.local/bin:$PATH" && cd /home/site/wwwroot && uv run fastapi run app.main:app --host 0.0.0.0 --port ${PORT:-8000}
 
 curl -LsSf https://astral.sh/uv/install.sh | sh && export PATH="/root/.local/bin:$PATH" && cd /home/site/wwwroot && uv run uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+
+curl -LsSf https://astral.sh/uv/install.sh | sh && export PATH="/root/.local/bin:$PATH" && cd /home/site/wwwroot/app && uv run fastapi run main.py --host 0.0.0.0 --port ${PORT:-8000}
